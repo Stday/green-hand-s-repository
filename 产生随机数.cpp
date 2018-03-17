3a9cbe4a75f22703fe891bd775299ef8197097d3
Stday <37464227+Stday@users.noreply.github.com>
2018-03-17 11:53:17 +0800

    Update 产生随机数.cpp

--- 产生随机数.cpp
+++ 产生随机数.cpp
@@ -1,11 +1,11 @@
 #include<stdio.h>
-//#include<time.h>
+#include<time.h>
 #include<stdlib.h>
 int main()
 {
 	int a[10];
 	int i;
-	//srand(time(0));
+	srand(time(0));
 	for(i=0;i<10;i++){ 
 	    a[i]=10+rand()%90;
  	     printf("%5d",a[i]);
