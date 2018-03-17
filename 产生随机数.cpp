34b6a9f0d2ad6c3fd405039ad312759df391d69e
Stday <37464227+Stday@users.noreply.github.com>
2018-03-17 11:44:41 +0800

    Add files via upload

---
+++ 产生随机数.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+//#include<time.h>
+#include<stdlib.h>
+int main()
+{
+	int a[10];
+	int i;
+	//srand(time(0));
+	for(i=0;i<10;i++){ 
+	    a[i]=10+rand()%90;
+ 	     printf("%5d",a[i]);
+ 	} 
+ 	return 0;
+}
